3e6c49fd77bf8aab0096f50b58beba822090f9c0
Vipul Tapare <vipul.tapare@diggibyte.com>
2024-09-30 14:39:35 +0530

    Added the Mini-Project

---
+++ src/app/Components/user-profile/user-profile.component.ts
@@ -0,0 +1,54 @@
+import { CommonModule } from '@angular/common';
+import { EventEmitter,booleanAttribute, Component, input, Input, numberAttribute, output, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+
+
+
+function formatName(val:string){
+   return "Hii, " + val;
+}
+
+
+@Component({
+  selector: 'app-user-profile',
+  standalone: true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css'
+})
+export class UserProfileComponent {
+  // name="Vipul"
+  // status="single"
+  // salary=40000
+  // isButton = false
+  // inputVal = "Vipul"
+
+  // users =[
+  //   {id:1,name:"Ramesh",isSingle:true,salary:42000},
+  //   {id:2,name:"Suresh",isSingle:false,salary:40000},
+  //   {id:3,name:"Jayesh",isSingle:true,salary:39000},
+  //   {id:4,name:"Natesh",isSingle:false,salary:52000},
+  //   {id:5,name:"Yogesh",isSingle:true,salary:40000}
+  // ]
+  // onChange(e:Event)
+  // {
+  //  // console.log((e?.target as HTMLInputElement).value);
+  // //  Two Way Data Binding
+  //  this.inputVal = (e. target as HTMLInputElement).value
+  // }
+
+  // Input Decorators
+  @Input({alias:"user-Name" , transform:formatName}) name = "";
+  @Input({transform:booleanAttribute}) isSingle!:boolean;
+  @Input({transform:numberAttribute}) salary!:number
+
+  // Output Decorators
+  @Output() myEvent = new EventEmitter<{name:string , newSalary:number}>()
+
+  sendData() {
+    
+    
+  }
+}
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserProfileComponent } from './Components/user-profile/user-profile.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,UserProfileComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'mini-Project';
+  users =[
+    {id:1,name:"Ramesh",isSingle:true,salary:42000},
+    {id:2,name:"Suresh",isSingle:false,salary:40000},
+    {id:3,name:"Jayesh",isSingle:true,salary:39000},
+    {id:4,name:"Natesh",isSingle:false,salary:52000},
+    {id:5,name:"Yogesh",isSingle:true,salary:40000}
+  ]
+
+  receivedMessage : string = "";
+
+  receivedData(val:string)
+  {
+     this.receivedMessage = val;
+     console.log(this.receivedMessage)
+  }
 }
